3f4caa2d7c3bc7c205e4f8d555d867c179706b36
sanjiv sahayam <sanjsmailbox@gmail.com>
2021-04-12 09:20:30 +1000

    Bump versions

--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 # Modify these when doing a new build
-BUILD_VERSION='v17'
+BUILD_VERSION='v18'
 SLATE_VERSION='0.1.2.0'
-SCRIB_VERSION='0.3.0.7'
+SCRIB_VERSION='0.3.0.8'
 IMAGE_VERSION="${BUILD_VERSION}-SL_${SLATE_VERSION}-SC_${SCRIB_VERSION}"
 
 # v8-SL_0.1.0.8_SC_0.1.0.0
